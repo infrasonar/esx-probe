c4a0acd60e9958797ade085465fef7da1a5a676f
Koos85 <koosjoosten@gmail.com>
2024-09-17 10:04:40 +0200

    lint

--- lib/check/host_vms.py
+++ lib/check/host_vms.py
@@ -54,7 +54,8 @@ async def check_host_vms(
         for device in vm['config'].hardware.device:
             if isinstance(device, vim.vm.device.VirtualDisk):
                 datastore_name = device.backing.datastore.name  # type: ignore
-                datastore = stores_lookup[device.backing.datastore]  # type: ignore
+                datastore = \
+                    stores_lookup[device.backing.datastore]  # type: ignore
                 if datastore_name not in virtual_storage_dct:
                     virtual_storage_dct[datastore_name] = {
                         'name': datastore_name,
